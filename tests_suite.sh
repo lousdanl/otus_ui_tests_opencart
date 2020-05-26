e726a58878b5e1c415800cfa584b161cea04f151
Lousdanl <grigoreva080295@gmail.com>
2020-05-27 02:02:38 +0300

    Fixes

--- tests_suite.sh
+++ tests_suite.sh
@@ -3,6 +3,6 @@
 sudo docker build -t opencart_tests ~/otus_ui_tests_opencart;
 sudo docker run --name opencart opencart_tests;
 sudo docker cp opencart:/app/allure_report ~/otus_ui_tests_opencart;
-sudo allure-2.7.0/bin/allure serve ~/otus_ui_tests_opencart/allure_report;
+sudo allure serve ~/otus_ui_tests_opencart/allure_report;
 sudo rm -rf allure_report;
-sudo docker system prune -a
+sudo docker system prune
